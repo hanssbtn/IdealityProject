bbbb5b237b926fc922fe7776d5d9ef23672d0f77
Hans Sebastian <hans.sebastian.work@gmail.com>
2024-11-01 05:41:24 +0700

    First commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,111 @@
+import org.jetbrains.kotlin.gradle.utils.IMPLEMENTATION
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.org.jetbrains.kotlin.android)
+    // Existing plugins
+    alias(libs.plugins.compose.compiler)
+}
+
+composeCompiler {
+    includeSourceInformation = true
+    reportsDestination = layout.buildDirectory.dir("compose_compiler")
+    stabilityConfigurationFile = rootProject.layout.projectDirectory.file("/app/src/main/java/resources/stability_config.conf")
+}
+
+android {
+    namespace = "com.example.idealityproject"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.idealityproject"
+        minSdk = 29
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildFeatures {
+        compose = true
+    }
+    dataBinding {
+        enable = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.15"
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    // Jetpack Compose BOM
+    val composeBom = platform("androidx.compose:compose-bom:2024.09.03")
+    implementation(composeBom)
+    // Jetpack Compose + Material3
+    androidTestImplementation(composeBom)
+    implementation(libs.androidx.material3)
+    // Jetpack Compose Preview
+    implementation(libs.androidx.ui.tooling.preview)
+    debugImplementation(libs.androidx.ui.tooling)
+    // Jetpack Compose UI test
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.test.manifest)
+    // Jetpack Compose Add full set of material icons
+    implementation(libs.androidx.material.icons.extended)
+    // Jetpack Compose Add window size utils
+    implementation(libs.androidx.adaptive)
+    // Jetpack Compose Integration with activities
+    implementation(libs.androidx.activity.compose)
+    // Jetpack Compose Integration with ViewModels
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    // Jetpack Compose Integration with LiveData
+    implementation(libs.androidx.runtime.livedata)
+    // ConstraintLayout
+    implementation(libs.androidx.constraintlayout.compose)
+
+    // ARCore
+    implementation(libs.core)
+    // SceneView
+    implementation(libs.sceneview)
+    // ARSceneView
+    implementation(libs.arsceneview)
+
+    // Firebase BOM
+    //noinspection UseTomlInstead
+    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
+    // Firebase Auth
+    implementation(libs.firebase.auth.ktx)
+    // FirebaseUI Auth
+    implementation(libs.androidx.credentials.play.services.auth)
+
+    // SignInButton
+    implementation(libs.google.services)
+
+    // Core libs
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ app/src/main/java/com/example/idealityproject/MainActivity.kt
@@ -0,0 +1,405 @@
+package com.example.idealityproject
+
+import android.content.pm.PackageManager
+import android.content.res.Configuration
+import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+import android.widget.Toast
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.google.ar.core.ArCoreApk
+import com.google.ar.core.ArCoreApk.Availability
+import com.google.ar.core.Config
+import dev.romainguy.kotlin.math.Float2
+import io.github.sceneview.ar.ARScene
+import io.github.sceneview.ar.arcore.createAnchorOrNull
+import io.github.sceneview.ar.node.AnchorNode
+import io.github.sceneview.gesture.MoveGestureDetector
+import io.github.sceneview.gesture.RotateGestureDetector
+import io.github.sceneview.gesture.ScaleGestureDetector
+import io.github.sceneview.node.Node
+import kotlinx.coroutines.delay
+import kotlin.system.exitProcess
+
+class MainActivity : AppCompatActivity() {
+    private var userRequestedInstall = true
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Main()
+            CheckAndInstallARCore()
+        }
+    }
+
+    @Composable
+    fun Main() {
+        // Define your composable functions here
+        ConstraintLayout (
+            Modifier
+                .fillMaxSize()
+                .padding(WindowInsets.systemBars.asPaddingValues())
+        ) {
+            // Add your UI elements here
+            val (left, right, up, down) = createRefs()
+
+            // Model list
+            val (list) = createRefs()
+
+            val listenerCreator = remember {
+                mutableStateOf<io.github.sceneview.gesture.GestureDetector.OnGestureListener?>(null)
+            }
+
+
+            ARScene(
+                modifier = Modifier
+                    .fillMaxSize(),
+                onGestureListener = listenerCreator.value,
+                sessionConfiguration = { sess, config ->
+                    config.depthMode = when (sess.isDepthModeSupported(Config.DepthMode.AUTOMATIC)) {
+                        true -> {
+                            Log.d("ARScene", "DepthMode = Automatic")
+                            Config.DepthMode.AUTOMATIC
+                        }
+                        false -> {
+                            Log.d("ARScene", "DepthMode = Disabled")
+                            Config.DepthMode.DISABLED
+                        }
+                    }
+                    config.updateMode = Config.UpdateMode.LATEST_CAMERA_IMAGE
+                    config.instantPlacementMode = Config.InstantPlacementMode.LOCAL_Y_UP
+                    config.lightEstimationMode = Config.LightEstimationMode.ENVIRONMENTAL_HDR
+                    config.planeFindingMode = Config.PlaneFindingMode.HORIZONTAL_AND_VERTICAL
+                    config.semanticMode = Config.SemanticMode.DISABLED
+                },
+                onSessionCreated = { session ->
+                    Log.d("ARScene", "Session created ($session)")
+                },
+                onSessionResumed = { session ->
+                    val orientation = when (this@MainActivity.resources.configuration.orientation) {
+                        Configuration.ORIENTATION_LANDSCAPE -> "Landscape"
+                        Configuration.ORIENTATION_PORTRAIT -> "Portrait"
+                        Configuration.ORIENTATION_UNDEFINED -> "Undefined"
+                        else -> ""
+                    }
+                    Log.d("ARScene", "Session resumed, orientation: $orientation")
+                },
+                onViewCreated = {
+                    Log.d("ARScene", "View created ($this)")
+
+                    val handleSingleTapConfirmed = { e: MotionEvent, node: Node? ->
+                        val result = this.hitTestAR(
+                            e.x,
+                            e.y,
+                            depthPoint = this.session?.isDepthModeSupported(Config.DepthMode.AUTOMATIC) ?: false
+                        ) { hitRes ->
+                            val anchor = hitRes.createAnchorOrNull()
+                            if (anchor != null) {
+                                val anchorNode = AnchorNode(engine = this.engine, anchor = anchor)
+                                this@ARScene.addChildNode(anchorNode)
+                                Log.d("ARScene", "added anchor node ${anchorNode.position}")
+                            }
+
+                            false
+                        }
+                        result
+                    }
+
+
+                    listenerCreator.value =
+                        object : io.github.sceneview.gesture.GestureDetector.OnGestureListener {
+                            override fun onContextClick(e: MotionEvent, node: Node?) {
+                                Log.d("ARScene", "onContextClick: Not yet implemented")
+                            }
+
+                            override fun onDoubleTap(e: MotionEvent, node: Node?) {
+                                Log.d("ARScene", "onDoubleTap: Not yet implemented")
+                            }
+
+                            override fun onDoubleTapEvent(e: MotionEvent, node: Node?) {
+                                Log.d("ARScene", "onDoubleTapEvent: Not yet implemented")
+                            }
+
+                            override fun onDown(e: MotionEvent, node: Node?) {
+                                Log.d("ARScene", "onDown: Not yet implemented")
+                            }
+
+                            override fun onFling(
+                                e1: MotionEvent?,
+                                e2: MotionEvent,
+                                node: Node?,
+                                velocity: Float2
+                            ) {
+                                Log.d("ARScene", "onFling: Not yet implemented")
+                            }
+
+                            override fun onLongPress(e: MotionEvent, node: Node?) {
+                                Log.d("ARScene", "onLongPress: Not yet implemented")
+                            }
+
+                            override fun onMove(
+                                detector: MoveGestureDetector,
+                                e: MotionEvent,
+                                node: Node?
+                            ) {
+                                Log.d("ARScene", "onMove: Not yet implemented")
+                            }
+
+                            override fun onMoveBegin(
+                                detector: MoveGestureDetector,
+                                e: MotionEvent,
+                                node: Node?
+                            ) {
+                                Log.d("ARScene", "onMoveBegin: Not yet implemented")
+                            }
+
+                            override fun onMoveEnd(
+                                detector: MoveGestureDetector,
+                                e: MotionEvent,
+                                node: Node?
+                            ) {
+                                Log.d("ARScene", "onMoveEnd: Not yet implemented")
+                            }
+
+                            override fun onRotate(
+                                detector: RotateGestureDetector,
+                                e: MotionEvent,
+                                node: Node?
+                            ) {
+                                Log.d("ARScene", "onRotate: Not yet implemented")
+                            }
+
+                            override fun onRotateBegin(
+                                detector: RotateGestureDetector,
+                                e: MotionEvent,
+                                node: Node?
+                            ) {
+                                Log.d("ARScene", "onRotateBegin Not yet implemented")
+                            }
+
+                            override fun onRotateEnd(
+                                detector: RotateGestureDetector,
+                                e: MotionEvent,
+                                node: Node?
+                            ) {
+                                Log.d("ARScene", "onRotateEnd: Not yet implemented")
+                            }
+
+                            override fun onScale(
+                                detector: ScaleGestureDetector,
+                                e: MotionEvent,
+                                node: Node?
+                            ) {
+                                Log.d("ARScene", "onScale: Not yet implemented")
+                            }
+
+                            override fun onScaleBegin(
+                                detector: ScaleGestureDetector,
+                                e: MotionEvent,
+                                node: Node?
+                            ) {
+                                Log.d("ARScene", "onScaleBegin: Not yet implemented")
+                            }
+
+                            override fun onScaleEnd(
+                                detector: ScaleGestureDetector,
+                                e: MotionEvent,
+                                node: Node?
+                            ) {
+                                Log.d("ARScene", "onScaleEnd: Not yet implemented")
+                            }
+
+                            override fun onScroll(
+                                e1: MotionEvent?,
+                                e2: MotionEvent,
+                                node: Node?,
+                                distance: Float2
+                            ) {
+                                Log.d("ARScene", "onScroll: Not yet implemented")
+                            }
+
+                            override fun onShowPress(e: MotionEvent, node: Node?) {
+                                Log.d("ARScene", "onShowPress: Not yet implemented")
+                            }
+
+                            override fun onSingleTapConfirmed(e: MotionEvent, node: Node?) {
+                                var res = handleSingleTapConfirmed(e, node)
+                                Toast.makeText(
+                                    context,
+                                    "onSingleTapUpConfirmed",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+
+                            override fun onSingleTapUp(e: MotionEvent, node: Node?) {
+                                Log.d("ARScene", "onSingleTapUp: Not yet implemented")
+                            }
+                        }
+
+
+                    Log.d("ARScene", "onGestureListener: ${this.onGestureListener.toString()}")
+                }
+            )
+        }
+    }
+
+    private fun getARCoreVersion(): String? {
+        return try {
+            val packageInfo = packageManager.getPackageInfo("com.google.ar.core", 0)
+            packageInfo.versionName
+        } catch (e: PackageManager.NameNotFoundException) {
+            null
+        }
+    }
+
+    @Composable
+    private fun CheckAndInstallARCore() {
+        val localContext = LocalContext.current
+        var availability = ArCoreApk.getInstance().checkAvailability(localContext)
+        if (availability == Availability.SUPPORTED_INSTALLED) return
+        val maxRetries = 10
+        val shouldRetry = remember { mutableStateOf(true) }
+        val title = "Error"
+        val showDialog = remember { mutableStateOf(true) }
+        val text = remember { mutableStateOf("") }
+        val confirmAction = remember { mutableStateOf<(() -> Unit)?>(null) }
+        while (true) {
+            LaunchedEffect(Unit) {
+                var currentRetryCount = 0
+                while (true) {
+                    availability = ArCoreApk.getInstance().checkAvailability(localContext)
+                    when (availability) {
+                        Availability.UNKNOWN_CHECKING -> {
+                            delay(250)
+                        }
+
+                        Availability.UNSUPPORTED_DEVICE_NOT_CAPABLE -> {
+                            text.value = "This device does not have adequate ARCore support."
+                            break
+                        }
+
+                        Availability.SUPPORTED_INSTALLED -> {
+                            Toast.makeText(
+                                localContext,
+                                "Successfully installed ARCore",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            showDialog.value = false
+                            break
+                        }
+
+                        Availability.UNKNOWN_TIMED_OUT -> {
+                            currentRetryCount++
+                            if (currentRetryCount == maxRetries) {
+                                text.value =
+                                    "Failed to check device compatibility (timed out). Would you like to try again?"
+                                confirmAction.value = {
+                                    Toast.makeText(localContext, "Retrying", Toast.LENGTH_SHORT)
+                                        .show()
+                                    shouldRetry.value = true
+                                }
+                                break
+                            }
+                        }
+
+                        Availability.SUPPORTED_NOT_INSTALLED -> {
+                            var resp = requestARCoreInstall()
+                            while (!resp) {
+                                if (!userRequestedInstall) {
+                                    text.value = "User declined installation."
+                                    return@LaunchedEffect
+                                }
+                                resp = requestARCoreInstall()
+                            }
+                            break
+                        }
+
+                        Availability.SUPPORTED_APK_TOO_OLD -> {
+                            text.value =
+                                "The current ARCore version is not supported (version ${getARCoreVersion()})"
+                            break
+                        }
+
+                        Availability.UNKNOWN_ERROR -> {
+                            text.value = "An unknown error occurred. Try again?"
+                            confirmAction.value = {
+                                shouldRetry.value = true
+                            }
+                            break
+                        }
+                    }
+                }
+            }
+            if (!showDialog.value) return
+            AlertDialog(
+                icon = null,
+                title = { Text(title) },
+                text = { Text(text.value) },
+                onDismissRequest = {
+                    showDialog.value = false
+                    cleanup()
+                    exitProcess(1)
+                },
+                confirmButton = {
+                    TextButton(
+                        onClick = {
+                            confirmAction.value?.invoke()
+                            showDialog.value = false
+                        }) {
+                        Text("Yes")
+                    }
+                },
+                dismissButton = {
+                    TextButton(
+                        onClick = {
+                            showDialog.value = false
+                            cleanup()
+                            exitProcess(1)
+                        }) {
+                        Text("No")
+                    }
+                },
+            )
+            if (!shouldRetry.value) return
+        }
+    }
+
+    private fun cleanup() {
+
+    }
+
+    private fun requestARCoreInstall(): Boolean {
+        try {
+            when (ArCoreApk.getInstance().requestInstall(this, userRequestedInstall)) {
+                ArCoreApk.InstallStatus.INSTALLED -> {
+                    // ARCore is installed, proceed with AR functionality
+                    return true
+                }
+                ArCoreApk.InstallStatus.INSTALL_REQUESTED -> {
+                    // ARCore installation requested, pause the app until installation completes
+                    userRequestedInstall = false
+                }
+            }
+        } catch (e: Exception) {
+            return false
+        }
+        return false
+    }
+}
